e3469e99dfa80bfb0f6927095d9a40964342e7bc
SoyeongChoi <33438557+SoyeongChoi@users.noreply.github.com>
2019-01-07 10:11:04 +0900

    Parameters to use for Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -24,9 +24,24 @@ sigma = 0.3;
 %
 
 
-
-
-
+Cval = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+Sval = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+min_error = 1;
+%SVMTRAIN(X, Y, C, kernelFunction, tol, max_passes)
+for i = 1:length(Cval),
+	for j = 1:length(Sval),
+		Ct = Cval(i);
+		St = Sval(j);
+		model = svmTrain(X,y,Ct,@(x1, x2) gaussianKernel(x1, x2, St)); %@(x1,x2)gaussianKernel is an anonymous function.
+		predictions = svmPredict(model, Xval);
+		predictions_error = mean(double(predictions ~= yval));
+		if predictions_error < min_error,
+			C = Cval(i);
+			sigma = Sval(j);
+			min_error = predictions_error;
+		end		
+	end
+end 
 
 
 % =========================================================================
